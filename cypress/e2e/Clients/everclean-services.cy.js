765448e9c65cf193f26f6b465ba0de53989d0889
tomwattsuk <tomwatts@economist.com>
2023-03-06 15:35:03 +0000

    removed duplicate client

--- cypress/e2e/Clients/everclean-services.cy.js
+++
@@ -1,7 +0,0 @@
-describe('https and uptime check', () => {
-  it('passes', () => {
-    cy.visit('www.everclean-services.co.uk')
-    cy.url().should('include', 'https');
-    // became client 24th Feb 2023
-  })
-})
