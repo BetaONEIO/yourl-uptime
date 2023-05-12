2f5af708fc81148f20a29a46bc89cad3c7e8aaa0
Tom Watts <thomas.watts@!eamrhfitness.com>
2023-05-12 13:48:57 +0100

    removed castle church for now as we don't run this doman

--- cypress/e2e/Clients/castlechurchorguk.cy.js
+++
@@ -1,7 +0,0 @@
-describe('https and uptime check', () => {
-  it('passes', () => {
-    cy.visit('www.castlechurch.org.uk')
-    cy.url().should('include', 'https');
-    // became client 1st Apr 2023
-  })
-})
